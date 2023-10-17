24ac7a99cfdad93ea5ed7709ef6eae6abe959a79
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-10-17 18:32:22 -0300

    first commit

---
+++ src/main/java/com/rafa/rafa/RafaApplication.java
@@ -0,0 +1,13 @@
+package com.rafa.rafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafa/rafa/RafaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafa.rafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
